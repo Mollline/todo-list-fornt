ce2d55b50097894acdc24a7c5f56c560022ad500
24HP7503 24HP7503 <24HP7503@G303-CC0155.local>
2024-06-20 17:00:17 +0800

    fixed front3

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,44 @@
 'use client'
 import { useGetAllNotesQuery } from "@/generated";
 import { TodoList } from "./_component/CustomTable";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { CircularProgress } from '@material-ui/core';
 export default function Home() {
   const [mode, setMode] = useState(false);
   const { data, loading, error, refetch } = useGetAllNotesQuery();
   const da = data?.getAllNotes;
+  const [daata, setDaata] = useState(null); // State to store fetched data
+  const [loaading, setLoaading] = useState(true); // State to manage loading state
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        // Example API endpoint
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+        const json = await response.json();
+        setDaata(json);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoaading(false); // Update loading state once fetch operation is complete
+      }
+    };
+
+    fetchData();
+  }, []);
 
   if (loading) {
-    return <div style={{ color: "black", fontSize: "40px" }}>This is loading</div>;
+    return <div style={{ background: 'black', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}> <div >
+      {loading ? (
+        <CircularProgress size={150} thickness={6} />
+      ) : (
+        <div>
+          <h2>Data:</h2>
+          <pre>{JSON.stringify(daata, null, 2)}</pre>
+        </div>
+      )}
+    </div></div>;
   }
 
   return (
